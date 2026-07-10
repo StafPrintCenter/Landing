8810ef2c4276cd1689b2231c7c2736673875059b
steveasterafovo <steveasterafovo@gmail.com>
2026-07-10 03:35:55 +0100

    ss

---
+++ src/components/pages/faqs/home/index.ts
@@ -0,0 +1,7 @@
+export { FaqHomeHeader } from "./Header";
+export { FaqHomeSearchBar } from "./SearchBar";
+export { FaqHomeFilters, FAQ_SORT_OPTIONS, FAQ_SORT_DIRECTIONS, type FaqSortOption, type FaqSortDirection } from "./Filters";
+export { FaqHomeMobileTrigger } from "./MobileTrigger";
+export { FaqHomeMobileSheet } from "./MobileSheet";
+export { FaqHomeResultsCount } from "./ResultsCount";
+export { FaqHomeGrid } from "./Grid";
