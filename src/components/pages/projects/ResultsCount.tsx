88191859bc08a4923f5d7625b6254babd319c209
steveasterafovo <steveasterafovo@gmail.com>
2026-07-10 12:49:27 +0100

    ss

--- src/components/pages/projects/ResultsCount.tsx
+++ src/components/pages/projects/ResultsCount.tsx
@@ -1,3 +1,4 @@
+// src/components/pages/projects/ResultsCount.tsx
 import { ResultsCountText } from "@/components/shared/ResultsCountText";
 
 interface RealisationHomeResultsCountProps {
@@ -9,12 +10,5 @@ interface RealisationHomeResultsCountProps {
 }
 
 export function RealisationHomeResultsCount(props: RealisationHomeResultsCountProps) {
-  return (
-    <ResultsCountText
-      {...props}
-      isAllValue="Tout"
-      unit={{ singular: "projet", plural: "projets" }}
-      className="mt-6 text-sm text-muted-foreground"
-    />
-  );
+  return <ResultsCountText {...props} unit={{ singular: "projet", plural: "projets" }} />;
 }
