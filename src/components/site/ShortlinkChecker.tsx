0504c69557adbfe7c47c6f120e69a385ad9fcc75
steveasterafovo <steveasterafovo@gmail.com>
2026-07-11 14:24:41 +0100

    s

--- src/components/site/ShortlinkChecker.tsx
+++ src/components/site/ShortlinkChecker.tsx
@@ -36,7 +36,7 @@ export function ShortlinkChecker() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-24 right-5 z-50">
       {open && (
         <div className="mb-3 w-72 rounded-2xl border border-border bg-card p-4 shadow-xl">
           <div className="flex items-center justify-between">
