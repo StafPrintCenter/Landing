c60b82376d24f26d5fc2886d15352de1766377e1
steveasterafovo <steveasterafovo@gmail.com>
2026-07-12 21:48:17 +0100

    s

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,3 +1,4 @@
 export { TrainningRegistration } from "./TrainningRegistration";
 export { SearchDialog } from "./SearchDialog";
 export { ShareModal } from "./ShareModal";
+export { SiteReportModal } from "./SiteReportModal";
