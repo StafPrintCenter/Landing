87eb3e1c567e3cd7f8d5322dfd8b7526c6640589
steveasterafovo <steveasterafovo@gmail.com>
2026-07-12 22:41:20 +0100

    ss

--- src/components/site/floatinghelp/index.ts
+++ src/components/site/floatinghelp/index.ts
@@ -1,6 +1,4 @@
-export { EmptyState } from "./EmptyState";
-export { FilterMobileSheet } from "./FilterMobileSheet";
-export { FilterMobileTrigger } from "./FilterMobileTrigger";
-export { ResultsCountText } from "./ResultsCountText";
-export { SearchBarField } from "./SearchBarField";
-export { SearchBarInput } from "./SearchBarInput";
+export { HelpMenuPanel } from "./HelpMenuPanel";
+export { ReportSection } from "./ReportSection";
+export { ShortlinkSection } from "./ShortlinkSection";
+export { WhatsAppSection } from "./WhatsAppSection";
