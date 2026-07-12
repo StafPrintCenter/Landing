d5ac9a66f2c6d758c6c613a0bbea3ead5c713bfb
steveasterafovo <steveasterafovo@gmail.com>
2026-07-12 21:57:53 +0100

    s

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,4 +3,5 @@ export { useArticlesStore } from "./useArticlesStore";
 export { useFaqsStore } from "./useFaqsStore";
 export { useFormationsStore } from "./useFormationsStore";
 export { useProjectsStore } from "./useProjectsStore";
+export { useFormationsStore } from "./useFormationsStore";
 export { useServicesStore } from "./useServicesStore";
