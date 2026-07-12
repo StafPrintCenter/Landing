f1467809c55ba7061a4eca025ef959f743293257
steveasterafovo <steveasterafovo@gmail.com>
2026-07-12 19:33:54 +0100

    dd

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -15,6 +15,7 @@ export function Footer() {
 
   return (
     <footer className="mt-24 border-t border-border bg-secondary text-secondary-foreground">
+      {/* Modification ici : md:grid-cols-5 pour garder des proportions équilibrées avec le col-span-2 */}
       <div className="container-x grid gap-10 py-14 md:grid-cols-4">
         <div>
           <Link to="/" className="flex items-center">
