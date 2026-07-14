2cfe08689d44a9b68038747fdee29fb820f7224b
steveasterafovo <steveasterafovo@gmail.com>
2026-07-14 03:24:04 +0100

    ss

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,4 @@
-export { resolveShortlink, createShortlink } from "./useShortlinksStore";
+export { resolveShortlink, createShortlink } from "./useAppointmentsStore";
 export { createResourceStore } from "./createResourceStore";
 export { useArticlesStore } from "./useArticlesStore";
 export { sendContactRequest, trackContactRequest } from "./useContactStore";
