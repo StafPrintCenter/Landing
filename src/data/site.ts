c8b06e801898267bdafc6b69701150b753cd59ea
steveasterafovo <steveasterafovo@gmail.com>
2026-07-16 12:44:14 +0100

    nn

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   name: "STAF PRINT CENTER",
-  slogan: 'L\'empreinte de votre succès',
+  slogan: "L'empreinte de votre succès",
   activity: 'Studio de design et d\'impression',
   manager: "Steve Aster Afovo",
   city: "Porto-Novo, Bénin",
