edc57d05562db796a92357fd129bb4dcb24185b0
steveasterafovo <steveasterafovo@gmail.com>
2026-07-19 22:07:14 +0100

    ss

---
+++ src/components/pages/reviews/index.ts
@@ -0,0 +1,4 @@
+export { ReviewHeader } from "./ReviewHeader";
+export { ReviewForm } from "./ReviewForm";
+export { ReviewNotFoundState } from "./states/NotFoundState";
+export { ReviewSubmittedState } from "./states/SubmittedState";
