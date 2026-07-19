a4030ee61242888d42346f029939ee093975be2b
steveasterafovo <steveasterafovo@gmail.com>
2026-07-19 11:44:46 +0100

    ss

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -2,3 +2,4 @@ export { TrainningRegistration } from "./TrainningRegistration";
 export { SearchDialog } from "./SearchDialog";
 export { ShareModal } from "./ShareModal";
 export { SiteReportModal } from "./SiteReportModal";
+export { NewsletterPromptModal } from "./NewsletterPromptModal";
