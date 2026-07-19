8dc470e7cb0650c903add559d77639c36f890cb4
steveasterafovo <steveasterafovo@gmail.com>
2026-07-19 11:40:33 +0100

    ss

--- src/components/site/SiteShell.tsx
+++ src/components/site/SiteShell.tsx
@@ -3,8 +3,12 @@ import { Toaster } from "sonner";
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
 import { CookieConsent, FloatingHelpMenu } from "./";
+import { NewsletterPromptModal } from "@/components/modal/NewsletterPromptModal";
+import { useNewsletterPrompt } from "@/hooks/use-newsletter-prompt";
 
 export function SiteShell({ children }: { children: ReactNode }) {
+  const { isOpen, close, handleSubscribed, handleRemindLater, handleNeverAgain } = useNewsletterPrompt();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Nav />
@@ -14,6 +18,14 @@ export function SiteShell({ children }: { children: ReactNode }) {
       <FloatingHelpMenu />
       <CookieConsent />
       <Toaster position="bottom-left" richColors closeButton />
+
+      <NewsletterPromptModal
+        isOpen={isOpen}
+        onClose={close}
+        onSubscribed={handleSubscribed}
+        onRemindLater={handleRemindLater}
+        onNeverAgain={handleNeverAgain}
+      />
     </div>
   );
 }
