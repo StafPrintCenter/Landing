f9b2b7a6ecee7644e4f032d790f0aee6a1bd8c3c
steveasterafovo <steveasterafovo@gmail.com>
2026-07-19 23:16:11 +0100

    s

--- src/components/pages/reviews/index.ts
+++ src/components/pages/reviews/index.ts
@@ -1,4 +1,7 @@
 export { ReviewHeader } from "./ReviewHeader";
 export { ReviewForm } from "./ReviewForm";
+export { ClientInfoFields } from "./ClientInfoFields";
+export { QuestionRenderer } from "./QuestionRenderer";
+
 export { ReviewNotFoundState } from "./states/NotFoundState";
 export { ReviewSubmittedState } from "./states/SubmittedState";
