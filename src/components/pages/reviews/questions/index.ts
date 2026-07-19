5238c20648ef26f3bf22f8798862207d171566f7
steveasterafovo <steveasterafovo@gmail.com>
2026-07-19 23:26:17 +0100

    ss

--- src/components/pages/reviews/questions/index.ts
+++ src/components/pages/reviews/questions/index.ts
@@ -1,8 +1,8 @@
-export * from "./BooleanQuestion";
-export * from "./ChoiceQuestion";
-export * from "./ContactQuestion";
-export * from "./DateQuestion";
-export * from "./FileQuestion";
-export * from "./NumberQuestion";
-export * from "./RatingQuestion";
-export * from "./TextQuestion";
+export { BooleanQuestion } from "./BooleanQuestion";
+export { ChoiceQuestion } from "./ChoiceQuestion";
+export { ContactQuestion } from "./ContactQuestion";
+export { DateQuestion } from "./DateQuestion";
+export { FileQuestion } from "./FileQuestion";
+export { NumberQuestion } from "./NumberQuestion";
+export { RatingQuestion } from "./RatingQuestion";
+export { TextQuestion } from "./TextQuestion";
