47ada1ab263ab6688e1e2cece3c296ccbf33f7cd
steveasterafovo <steveasterafovo@gmail.com>
2026-07-24 14:41:36 +0100

    d

---
+++ src/components/modal/training-registration/index.ts
@@ -0,0 +1,7 @@
+export { ProgressBar } from "./ProgressBar";
+export { CharacterCounter } from "./CharacterCounter";
+export { StepContact } from "./StepContact";
+export { StepAvailability } from "./StepAvailability";
+export { StepSummary } from "./StepSummary";
+export { SuccessScreen } from "./SuccessScreen";
+export { registerSchema, type RegisterInput } from "./types";
