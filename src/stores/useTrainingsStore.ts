03d801dd67fe265bfbe3073c06eb0815dfec4be5
steveasterafovo <steveasterafovo@gmail.com>
2026-07-24 14:08:36 +0100

    s

---
+++ src/stores/useTrainingsStore.ts
@@ -0,0 +1,16 @@
+import { createResourceStore } from "./createResourceStore";
+import { type APIFormation } from "@/data/trainings";
+
+const { fetchList, fetchById, useResourceStore } = createResourceStore<APIFormation>({
+  resourceKey: "formations",
+  listEndpoint: "trainings/list",
+  detailEndpoint: "trainings",
+});
+
+export const fetchPublicFormations = fetchList;
+export const fetchFormationById = fetchById;
+
+export function useFormationsStore(params: Parameters<typeof useResourceStore>[0] = {}) {
+  const { data, ...rest } = useResourceStore(params);
+  return { formations: data, ...rest };
+}
