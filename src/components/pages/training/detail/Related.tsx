79a411b8b9f63c1e0b8ecff2c96b4724718a1c78
steveasterafovo <steveasterafovo@gmail.com>
2026-07-24 13:59:04 +0100

    d

--- src/components/pages/training/detail/Related.tsx
+++ src/components/pages/training/detail/Related.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import { type APIFormation } from "@/data/formations";
+import { type APIFormation } from "@/data/trainings";
 
 interface FormationRelatedProps {
   related: APIFormation[];
