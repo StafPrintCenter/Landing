c6e083f6b1a49c4ae88ef5d121f05518768dc714
steveasterafovo <steveasterafovo@gmail.com>
2026-07-28 17:41:56 +0100

    c

--- src/components/pages/careers/internships/index.ts
+++ src/components/pages/careers/internships/index.ts
@@ -1,3 +1,4 @@
 export { InternshipRequestForm } from "./RequestForm";
 export { SuccessScreen } from "./SuccessScreen";
 export { InternshipResult } from "./InternshipResult";
+export { InternshipSidebar } from "./Sidebar";
