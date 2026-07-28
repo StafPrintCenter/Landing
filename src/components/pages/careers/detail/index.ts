d4c538fb0bf07ed309c30e915801824fcade9167
steveasterafovo <steveasterafovo@gmail.com>
2026-07-28 16:32:09 +0100

    s

--- src/components/pages/careers/detail/index.ts
+++
@@ -1,4 +0,0 @@
-export { JobOfferDetailHeader } from "./Header";
-export { JobOfferDetailBody } from "./Body";
-export { JobOfferDetailSidebar } from "./Sidebar";
-export { JobOfferNotFoundState } from "./NotFoundState";
