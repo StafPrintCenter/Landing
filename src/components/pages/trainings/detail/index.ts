c969280331ce4d3218582aebcfc1bdd478bfc36a
steveasterafovo <steveasterafovo@gmail.com>
2026-07-29 18:43:53 +0100

    x

---
+++ src/components/pages/trainings/detail/index.ts
@@ -0,0 +1,7 @@
+export { FormationDetailHeader } from "./Header";
+export { FormationDetailSkeleton } from "./Skeleton";
+export { FormationDetailProgram } from "./Program";
+export { FormationDetailSidebar } from "./Sidebar";
+export { FormationDetailRelated } from "./Related";
+export { FormationDetailNotFound } from "./NotFound";
+export { FormationDetailError } from "./Error";
