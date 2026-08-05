c422c00235d9292f3ff4fe4e8122a0a9b5c30548
steveasterafovo <steveasterafovo@gmail.com>
2026-08-05 10:31:14 +0100

    s

--- src/routes/legal/privacy.tsx
+++ src/routes/legal/privacy.tsx
@@ -216,8 +216,7 @@ function getSections(): LegalSection[] {
               <span className="font-bold">
                 Absence de traçage tiers :
               </span>{" "}
-              Aucun cookie publicitaire, outil de profilage ou dispositif de suivi
-              comportemental tiers n'est utilisé à votre insu à des fins commerciales.
+              Aucun cookie publicitaire, outil de profilage ou dispositif de suivi              comportemental tiers n'est utilisé à votre insu à des fins commerciales.
               Vous pouvez à tout moment configurer votre navigateur afin de bloquer,
               limiter ou supprimer les cookies enregistrés.
             </li>
