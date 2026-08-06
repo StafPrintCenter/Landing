6377d048a496b4313f429bf6c7c2445d072143dc
steveasterafovo <steveasterafovo@gmail.com>
2026-08-06 08:11:38 +0100

    z

--- src/routes/legal/privacy.tsx
+++ src/routes/legal/privacy.tsx
@@ -117,7 +117,7 @@ function getSections(): LegalSection[] {
               </span>{" "}
               Analyse des performances de nos campagnes d'information (vues, taux de clic, fermetures des annonces) ainsi que mesure du trafic acheminé via notre service de redirection{" "}
               <code className="rounded bg-muted px-1.5 py-0.5 text-[13px]">
-                go.stafprint.com
+                {SITE.shortUrl}
               </code>.
             </li>
           </ol>
