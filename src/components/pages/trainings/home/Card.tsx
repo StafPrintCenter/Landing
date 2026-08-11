3a70381cfc4e84fae95d0c0c41ba77adc30332f3
steveasterafovo <steveasterafovo@gmail.com>
2026-08-11 01:34:25 +0100

    s

--- src/components/pages/trainings/home/Card.tsx
+++ src/components/pages/trainings/home/Card.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { Link } from "@tanstack/react-router";
 import { Clock, BarChart3, ArrowRight, Users, MapPin, Calendar } from "lucide-react";
-import { getThemeColor, type APIFormation } from "@/data/trainings";
+import { formatDate, getThemeColor, type APIFormation } from "@/data/trainings";
 
 interface FormationCardProps {
   formation: APIFormation;
