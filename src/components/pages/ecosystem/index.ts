feac17878250689b189e0eca3cd79bd3bf0aff67
steveasterafovo <steveasterafovo@gmail.com>
2026-08-19 21:58:39 +0100

    l

--- src/components/pages/ecosystem/index.ts
+++
@@ -1,4 +0,0 @@
-export { EcosystemHeader } from "./Header";
-export { EcosystemGrid } from "./Grid";
-export { EcosystemCard } from "./Card";
-export { EcosystemFilters, type EcosystemSortDirection } from "./Filters";
