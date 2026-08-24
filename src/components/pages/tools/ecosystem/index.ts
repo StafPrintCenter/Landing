1096577907ede20f7542964aaad1d8ca8fb014f6
steveasterafovo <steveasterafovo@gmail.com>
2026-08-24 11:13:34 +0100

    s

--- src/components/pages/tools/ecosystem/index.ts
+++ src/components/pages/tools/ecosystem/index.ts
@@ -1,7 +1,7 @@
 export { EcosystemHeader } from "./Header";
 export { EcosystemGrid } from "./Grid";
 export { EcosystemCard } from "./Card";
-export { EcosystemFilters, type EcosystemSortDirection } from "./Filters";
+export { EcosystemFilters, type EcosystemSortOption } from "./Filters";
 export { EcosystemMobileTrigger } from "./MobileTrigger";
 export { EcosystemMobileSheet } from "./MobileSheet";
 export { EcosystemSearchBar } from "./SearchBar";
