6f8974bdbabdfe76105da090f0d6f47c5385aa67
steveasterafovo <steveasterafovo@gmail.com>
2026-09-01 16:45:12 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,4 +1,5 @@
 export { FloatingHelpMenu } from "./FloatingHelpMenu";
 export { CookieConsent } from "./CookieConsent";
+export { ThemeToggle } from "./ThemeToggle";
 export { Nav } from "./Nav";
 export { Footer } from "./Footer";
