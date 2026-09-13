c7e6ce377f81ffd7179544d6dea387f840663761
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 00:14:43 +0100

    s

--- src/components/pages/tools/ecosystem/index.ts
+++ src/components/pages/tools/ecosystem/index.ts
@@ -1,7 +1,7 @@
 export { EcosystemHeader } from "./Header";
 export { EcosystemGrid } from "./Grid";
 export { EcosystemCard } from "./Card";
-export { EcosystemFilters, type EcosystemSortOption } from "./Filters";
+export { EcosystemFilters, ECOSYSTEM_SORT_OPTIONS, ECOSYSTEM_SORT_DIRECTIONS, type EcosystemSortOption, type EcosystemSortDirection } from "./Filters";
 export { EcosystemMobileTrigger } from "./MobileTrigger";
 export { EcosystemMobileSheet } from "./MobileSheet";
 export { EcosystemSearchBar } from "./SearchBar";
