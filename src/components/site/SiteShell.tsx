7c20025007169ebf18c93d1345226d6deab9d305
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 11:18:14 +0100

    s

--- src/components/site/SiteShell.tsx
+++ src/components/site/SiteShell.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { Nav, Footer, CookieConsent, FloatingHelpMenu } from "./";
+import { Header, Footer, CookieConsent, FloatingHelpMenu } from "./";
 import { NewsletterPromptModal } from "@/components/modal/NewsletterPromptModal";
 import { useNewsletterPrompt } from "@/hooks/use-newsletter-prompt";
 import { AnnouncementsHost } from "@/components/site/announcements/Host";
@@ -9,7 +9,7 @@ export function SiteShell({ children }: { children: ReactNode }) {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Nav />
+      <Header />
       <main className="flex-1 pt-16 md:pt-20">{children}</main>
       <Footer />
 
