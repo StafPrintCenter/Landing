9c2cc93a787e6ad63a1a4b835302f5baac0b7a4d
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 11:18:09 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -2,5 +2,5 @@ export { FloatingHelpMenu } from "./FloatingHelpMenu";
 export { CookieConsent } from "./CookieConsent";
 export { ThemeToggle } from "./ThemeToggle";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
-export { Nav } from "./Nav";
+export { Header } from "./Header";
 export { Footer } from "./Footer";
