92d5809de4b0822b550d69fadfb64d30c384f03f
steveasterafovo <steveasterafovo@gmail.com>
2026-09-18 10:05:47 +0100

    s

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -125,7 +125,7 @@ export const Route = createFileRoute("/sitemap.xml")({
         return new Response(xml, {
           headers: {
             "Content-Type": "application/xml",
-            "Cache-Control": "public, max-age=3600"
+            "Cache-Control": "public, max-age=3600",
           },
         });
       },
